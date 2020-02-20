48f352eaadc0bccecb96739eb7f50057b7b00661
Keith Ng <keith.jk.ng@gmail.com>
2020-02-20 15:49:20 -0800

    working backbones but faulty ruleset

--- site/buttons.js
+++ site/buttons.js
@@ -1,23 +1,54 @@
 let input = [];
 
+function buttonPress() {
 $('.simon-button.green').on('click', function() {
   input.push(0);
-  alert(`Input is now ${input}`);
+  console.log(`Input is now ${input}`);
+  gameCheck()
 });
 
 $('.simon-button.red').on('click', function() {
   input.push(1);
-  alert(`Input is now ${input}`);
+  console.log(`Input is now ${input}`);
+  gameCheck()
 });
 
 $('.simon-button.yellow').on('click', function() {
   input.push(2);
-  alert(`Input is now ${input}`);
+  console.log(`Input is now ${input}`);
+  gameCheck()
 });
 
 $('.simon-button.blue').on('click', function() {
   input.push(3);
-  alert(`Input is now ${input}`);
+  console.log(`Input is now ${input}`);
+  gameCheck()
 });
 
-module.exports = input;
+}
+
+let arr = [];
+let count = 0;
+
+function pattern() {
+  let randNum = Math.floor(Math.random() * 4);
+  arr.push(randNum);
+  console.log(`The generated array is ${arr}`);
+}
+
+let arrStr = '';
+let inputStr = '';
+
+function gameCheck() {
+  arrStr = arr.toString('');
+  inputStr = input.toString('');
+  if (arrStr === inputStr) {
+    pattern();
+    count += 1;
+  }
+  else {
+    console.log(`You lost after ${count} turns!`);
+  }
+}
+pattern();
+buttonPress();
